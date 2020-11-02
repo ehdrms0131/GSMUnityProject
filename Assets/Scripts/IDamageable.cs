4a29e370a84292e979d8e98e67c49fa1916cdea9
jackdraw111 <jackdraw111@kakao.com>
2020-11-02 22:52:47 +0900

    UnityFile
    Unity

---
+++ Assets/Scripts/IDamageable.cs
@@ -0,0 +1,12 @@
+﻿              
+using UnityEngine;
+
+
+
+    // Start is called before the first frame update
+  
+    public interface IDamageable
+    {
+        void TakeHit(float damage, RaycastHit2D hit);
+    }
+
